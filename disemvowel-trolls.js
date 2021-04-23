51a227d8028c3b8b0a89a98d29ea98074656dd81
Monica Goncalves <monicagoncalves.Dev@gmail.com>
2021-04-22 23:15:36 -0400

    corrected instructions

--- disemvowel-trolls.js
+++ disemvowel-trolls.js
@@ -1,8 +1,12 @@
-/* Return the number (count) of vowels in the given string.
+/* Trolls are attacking your comment section!
 
-We will consider a, e, i, o, u as vowels for this Kata (but not y).
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
 
-The input string will only consist of lower case letters and/or spaces.
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
 
  */
 
